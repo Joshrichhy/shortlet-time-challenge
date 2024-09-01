4534f42bbfd95404cc303f3be9b13fc4aeba610f
apple <kusejoshua@gmail.com>
2024-09-01 19:35:36 +0100

    CICD and terraform

--- terraform/gke-cluster/output.tf
+++ terraform/gke-cluster/output.tf
@@ -1,14 +1,14 @@
 output "cluster_name" {
-  value = google_container_cluster.primary.name
+  value = google_container_cluster.gke_cluster.name
 }
 
 output "cluster_endpoint" {
-  value = google_container_cluster.primary.endpoint
+  value = google_container_cluster.gke_cluster.endpoint
 }
 
-output "service_ip" {
-  value = kubernetes_service.time_service.status[0].load_balancer[0].ingress[0].ip
-}
+# output "service_ip" {
+#   value = kubernetes_service.time_service.status[0].load_balancer[0].ingress[0].ip
+# }
 
 
 output "cluster_zone" {
--- terraform/kubernetes-resources/output.tf
+++ terraform/kubernetes-resources/output.tf
@@ -1,10 +1,10 @@
-output "cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
-output "cluster_endpoint" {
-  value = google_container_cluster.primary.endpoint
-}
+# output "cluster_name" {
+#   value = google_container_cluster.primary.name
+# }
+#
+# output "cluster_endpoint" {
+#   value = google_container_cluster.primary.endpoint
+# }
 
 output "service_ip" {
   value = kubernetes_service.time_service.status[0].load_balancer[0].ingress[0].ip
